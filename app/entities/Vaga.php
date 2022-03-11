557531d44cdbe803971321bf64b20a6285a2987b
Leonardo Falkemberg Huttner <20172CMQ.ADS0191@ACADEMICO.CAMAQUA>
2022-03-10 22:03:47 -0300

    criação conexão

---
+++ app/entities/Vaga.php
@@ -0,0 +1,13 @@
+<?php
+    class Vaga{
+        private $id;
+        private $titulo;
+        private $descricao;
+        private $tipo;
+        private $empresa;
+
+        public function __construct($titulo=null, $descricao=null, $tipo=null, $empresa=null){
+            
+        }
+    }
+?>
